d47605a9a5d26dbad00cd320521d069f3ec33ed4
Piso <pisodev0427@gmail.com>
2023-10-25 13:28:04 +0900

    feat: Todo page interval event
    - Todo
    	- remove change event; unuse temporary save.
    	- add useeffect for temporary save every certain time.
    
    - TodoAPI
    	- organize name of variables and layouts

--- src/feats/Todo.jsx
+++ src/feats/Todo.jsx
@@ -1,47 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { saveTodos } from './TodosAPI';
 
 import Alarm from './Alarm';
 
+/**
+ * Todo가 9시부터 10시까지 고정.
+ * So; 배열을 todos란 변수에 할당
+ * */
+const TODOS = [
+    { time: 9, todo: '' },
+    { time: 10, todo: '' },
+    { time: 11, todo: '' },
+    { time: 12, todo: '' },
+    { time: 13, todo: '' },
+    { time: 14, todo: '' },
+    { time: 15, todo: '' },
+    { time: 16, todo: '' },
+    { time: 17, todo: '' },
+];
+
 export default function Todo() {
-    const [isAlarm, setIsAlarm] = useState(false);
+    const [state, setState] = useState({ isAlarm: false, todos: TODOS });
 
-    /**
-     * Todo가 9시부터 10시까지 고정.
-     * So; 배열을 todos란 변수에 할당
-     * */
-    const todos = [
-        { time: 9, todo: '' },
-        { time: 10, todo: '' },
-        { time: 11, todo: '' },
-        { time: 12, todo: '' },
-        { time: 13, todo: '' },
-        { time: 14, todo: '' },
-        { time: 15, todo: '' },
-        { time: 16, todo: '' },
-        { time: 17, todo: '' },
-    ];
-
-    const handleTodosChange = (e) => {
-        // 이벤트가 발생시 임시저장 처럼 자동적으로 저장
-    };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('wjwkd');
+        }, 1000000);
+        return () => clearInterval(interval);
+    }, []);
 
     /**
      * Todo를 저장하는 이벤트를 담당하는 함수
+     * 1. Form에서 Todo들을 모아서 배열화한다.
+     * 2. Todos 상태를 업데이트 후, 서버(스토리지)에 저장한다.
+     * 3. 알람을 띄운다.
      * @param {*} e
      */
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        setIsAlarm(true);
-        saveTodos([...e.target.todo].map((todo, idx) => ({ time: idx + 9, todo: todo.value })));
+        const newTodos = [...e.target.todo].map((todo, idx) => ({ time: idx + 9, todo: todo.value }));
+        saveTodos(newTodos);
+        setState({
+            isAlarm: true,
+            todos: newTodos,
+        });
+    };
+
+    const { isAlarm, todos } = state;
+    const setIsAlarm = (isAlarm) => {
+        setState({ ...state, isAlarm });
     };
 
     return (
         <>
             <h1>Welcome Todo Page</h1>
             <Alarm isAlarm={isAlarm} setIsAlarm={setIsAlarm} />
-            <form onChange={handleTodosChange} onSubmit={handleFormSubmit}>
+            <form onSubmit={handleFormSubmit}>
                 <button type="submit" disabled={isAlarm}>
                     오늘 Todos 저장하기
                 </button>
--- src/feats/TodosAPI.jsx
+++ src/feats/TodosAPI.jsx
@@ -1,11 +1,17 @@
+const todos = {
+    get todayId() {
+        const date = new Date();
+        return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+    },
+};
+
 export const getTodosList = () => {
     const todoList = localStorage.getItem('todoList');
     return todoList ? JSON.parse(todoList) : [];
 };
 
 export const saveTodos = (newTodos) => {
-    const date = new Date();
-    const newTodosId = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+    const newTodosId = todos.todayId;
     const todosList = getTodosList();
 
     const updatedTodoList = todosList.map((todos) => (todos.id === newTodosId ? { ...todos, todos: newTodos } : todos));
