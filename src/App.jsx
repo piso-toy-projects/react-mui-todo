e3772499d62e6e3488bb3c72719f09272614400d
Piso <pisodev0427@gmail.com>
2023-10-26 15:54:17 +0900

    feat: M.U.I stlying to Todo
    - App
    	- Applying DarkMode
    
    - TOdo
    	- styling with mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import { ThemeProvider, createTheme } from '@mui/material';
+import { useMediaQuery, CssBaseline } from '@mui/material/';
+
 import Todo from './feats/Todo';
 import Todos from './feats/Todos';
+import { useState } from 'react';
+
+const lightTheme = createTheme();
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
+    const [theme, setTheme] = useState(useMediaQuery('(prefers-color-scheme: dark)') ? darkTheme : lightTheme);
+
+    const toggleTheme = () => {
+        setTheme(theme === darkTheme ? lightTheme : darkTheme);
+    };
+
     return (
         <BrowserRouter>
-            <Link to={'/'}>Todo</Link>
-            <Link to={'/todos'}>Todos</Link>
-
-            <Routes>
-                <Route path={'/'} element={<Todo />} />
-                <Route path={'/todos'} element={<Todos />} />
-            </Routes>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Link to={'/'}>Todo</Link>
+                <Link to={'/todos'}>Todos</Link>
+                <button onClick={toggleTheme}>ss</button>
+                <Routes>
+                    <Route path={'/'} element={<Todo />} />
+                    <Route path={'/todos'} element={<Todos />} />
+                </Routes>
+            </ThemeProvider>
         </BrowserRouter>
     );
 }
--- src/feats/Todo.jsx
+++ src/feats/Todo.jsx
@@ -1,32 +1,23 @@
 import { useEffect, useState } from 'react';
 
-import { saveTodos } from './TodosAPI';
+import { saveTodos, getTodayTodos } from './TodosAPI';
+
+import { Box, Checkbox, List, Typography, ListItem, ListItemText, TextField, Button } from '@mui/material';
+import { NoteAlt, SaveAlt } from '@mui/icons-material';
 
 import Alarm from './Alarm';
 
-/**
- * Todo가 9시부터 10시까지 고정.
- * So; 배열을 todos란 변수에 할당
- * */
-const TODOS = [
-    { time: 9, todo: '' },
-    { time: 10, todo: '' },
-    { time: 11, todo: '' },
-    { time: 12, todo: '' },
-    { time: 13, todo: '' },
-    { time: 14, todo: '' },
-    { time: 15, todo: '' },
-    { time: 16, todo: '' },
-    { time: 17, todo: '' },
-];
-
 export default function Todo() {
-    const [state, setState] = useState({ isAlarm: false, todos: TODOS });
+    const [isAlarm, setIsAlarm] = useState(false);
+    const [todos, setTodos] = useState([]);
 
     useEffect(() => {
+        setTodos(getTodayTodos());
+
         const interval = setInterval(() => {
             console.log('wjwkd');
-        }, 1000000);
+        }, 600000);
+
         return () => clearInterval(interval);
     }, []);
 
@@ -39,39 +30,70 @@ export default function Todo() {
      */
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        const newTodos = [...e.target.todo].map((todo, idx) => ({ time: idx + 9, todo: todo.value }));
+
+        const checks = [...e.target.todocheck];
+        const todoStr = [...e.target.todo];
+
+        const newTodos = todos.map((_, idx) => ({
+            time: idx + 9,
+            todo: todoStr[idx < 3 ? idx : idx - 1].value,
+            checked: checks[idx < 3 ? idx : idx - 1].checked,
+        }));
+
+        setIsAlarm(true);
+        setTodos(newTodos);
         saveTodos(newTodos);
-        setState({
-            isAlarm: true,
-            todos: newTodos,
-        });
     };
 
-    const { isAlarm, todos } = state;
-    const setIsAlarm = (isAlarm) => {
-        setState({ ...state, isAlarm });
+    const handleChange = (e) => {
+        const checkdTodoId = +e.target.dataset.id;
+
+        const updatedTodos = todos.map((todo) =>
+            todo.time === checkdTodoId ? { ...todo, checked: e.target.checked } : todo
+        );
+
+        setTodos(updatedTodos);
     };
+
+    const formatNumToTime = (time) => `${('' + time).padStart(2, '0')}:00 ~ ${('' + (time + 1)).padStart(2, '0')}:00`;
 
     return (
-        <>
-            <h1>Welcome Todo Page</h1>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+            <Typography variant="h2" component="h1">
+                Welcome Todo Page
+            </Typography>
             <Alarm isAlarm={isAlarm} setIsAlarm={setIsAlarm} />
-            <form onSubmit={handleFormSubmit}>
-                <button type="submit" disabled={isAlarm}>
-                    오늘 Todos 저장하기
-                </button>
-                <ul>
-                    {todos.map(({ time, todo }) =>
-                        // prettier-ignore
-                        <li key={time}>
-                            <h2> 
-                                { `${('' + (time)).padStart(2,'0')}:00 ~ ${('' + (time + 1)).padStart(2,'0')}:00` }
-                            </h2>
-                            { time === 12 ? <p>중식</p> : <input name="todo" type='text' maxLength={100} defaultValue={todo} data-id={time}/> }
-                        </li>
-                    )}
-                </ul>
+            <form onSubmit={handleFormSubmit} onChange={handleChange} style={{ width: '80%' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        endIcon={<SaveAlt />}
+                        disabled={isAlarm}
+                        sx={{ alignSelf: 'end' }}
+                    >
+                        오늘 Todos 저장하기
+                    </Button>
+                    <List sx={{ width: 1 }}>
+                        {todos.map(({ time, todo, checked }) =>
+                            // prettier-ignore
+                            time === 12 
+                            ? <ListItem key={time}><ListItemText primary={'중식'}></ListItemText></ListItem>
+                            : <ListItem 
+                                key={time}
+                                secondaryAction={<Checkbox edge='end' name='todocheck' inputProps={{'data-id': time}} checked={checked}/>}>
+                                    <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '100%'}}>
+                                        <NoteAlt sx={{ color: 'action.active', mr: 1, my: 0.5, fontSize:'3rem' }} />
+                                        <TextField 
+                                            id="input-with-sx" name="todo" label={formatNumToTime(time)}
+                                            sx={{flexGrow:1}} variant='outlined'
+                                            maxLength={100} defaultValue={todo} disabled={checked} />
+                                    </Box>
+                            </ListItem>
+                        )}
+                    </List>
+                </Box>
             </form>
-        </>
+        </Box>
     );
 }
