abe82ea928c6eeb20ee16055f5c57dbe88a82182
Piso <pisodev0427@gmail.com>
2023-10-27 14:26:50 +0900

    refactor: add comments"

--- src/feats/Links.jsx
+++ src/feats/Links.jsx
@@ -3,9 +3,20 @@ import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { Box, IconButton, Link, Typography } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 
+/**
+ * Links component for 헤더와 라이트/다크 토글
+ * @param {function} props.toggleTheme - Function to toggle the theme.
+ * @param {object} props.theme - Current theme object.
+ * @returns {JSX.Element} React component.
+ */
 export default function Links({ toggleTheme, theme }) {
+    /** @type {string} */
     const { pathname } = useLocation();
 
+    /**
+     * - to: Route의 path 이름
+     * - label: 링크 텍스트
+     * @type {{to: string, label: string}} */
     const links = [
         { to: '/', label: 'Todo' },
         { to: '/todos', label: 'Todos' },
