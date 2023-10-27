16fea2266620d50133cf0e3050a0b37f56719d6e
Piso <pisodev0427@gmail.com>
2023-10-27 10:34:45 +0900

    feat: Link text font changed
    - string to Typography from MUI

--- src/feats/Links.jsx
+++ src/feats/Links.jsx
@@ -1,6 +1,6 @@
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 
-import { Box, IconButton, Link } from '@mui/material';
+import { Box, IconButton, Link, Typography } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 
 export default function Links({ toggleTheme, theme }) {
@@ -9,10 +9,14 @@ export default function Links({ toggleTheme, theme }) {
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '2rem' }}>
             <Link component={RouterLink} to={'/'} underline={pathname === '/' ? 'always' : 'hover'}>
-                Todo
+                <Typography variant="h3" component={'span'}>
+                    Todo
+                </Typography>
             </Link>
             <Link component={RouterLink} to={'/todos'} underline={pathname === '/todos' ? 'always' : 'hover'}>
-                Todos
+                <Typography variant="h3" component={'span'}>
+                    Todos
+                </Typography>
             </Link>
 
             <IconButton onClick={toggleTheme} color="inherit">
